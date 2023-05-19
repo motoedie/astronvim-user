a02e595bacdc130f9e940a53a0ea927d3c63769c
= <a@a.com>
2023-05-19 22:23:42 +0200

    Use different surround package

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -17,7 +17,7 @@ return {
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.lsp.inc-rename" },
   { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
