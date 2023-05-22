ac6b421085409752a278a3d29a45722b166ea239
= <a@a.com>
2023-05-22 14:31:37 +0200

    Fix fugitive to load all commands

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -28,11 +28,6 @@ return {
   },
   {
     'tpope/vim-fugitive',
-    opt = true,
-    cmd = {
-      "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit",
-      "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
-      "Gdelete", "Gremove", "Gbrowse"
-    },
+    lazy = false,
   },
 }
