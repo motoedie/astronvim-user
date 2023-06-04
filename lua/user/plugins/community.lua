14aab96cac739d9b6eacd0e8549c5d550348652c
= <a@a.com>
2023-06-04 23:32:51 +0200

    Update inc-rename plugin name

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -15,7 +15,7 @@ return {
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-  { import = "astrocommunity.lsp.inc-rename" },
+  { import = "astrocommunity.lsp.inc-rename-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.markdown" },
