54ed22af6f0e6330c48e565f51e24d6f6d24e31a
= <a@a.com>
2023-06-09 22:27:18 +0200

    Remove LSP inlay hints

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -16,7 +16,6 @@ return {
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
-  { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.json" },
