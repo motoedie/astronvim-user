69ebc0339d7c9f2600dec37b09b855c709b0f044
= <a@a.com>
2023-06-13 00:36:33 +0200

    Update unused variable color

--- lua/user/plugins/tokyonight.lua
+++ lua/user/plugins/tokyonight.lua
@@ -5,6 +5,9 @@ return {
       hl.CursorLine = {
         bg = c.bg_visual,
       }
+      hl.DiagnosticUnnecessary = {
+        fg = c.comment,
+      }
     end,
     on_colors = function(colors)
       colors.bg_statusline = colors.none
