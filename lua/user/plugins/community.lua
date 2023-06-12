5f2722f0038645bd32251540545ec85e1d733080
= <a@a.com>
2023-06-12 12:08:09 +0200

    Add hop plugin to quickly navigate in document

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -15,6 +15,7 @@ return {
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.lsp.inc-rename-nvim" },
+  { import = "astrocommunity.motion.hop-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.json" },
