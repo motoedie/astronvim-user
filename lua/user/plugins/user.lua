b4fc74aee49b8a46423fac9c703a2c156c43029f
= <a@a.com>
2023-07-24 01:41:42 +0200

    Use lsp_signature plugin from astro community

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -14,13 +14,6 @@ return {
     end,
   },
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
-  },
-  {
     'tpope/vim-fugitive',
     lazy = false,
   },
