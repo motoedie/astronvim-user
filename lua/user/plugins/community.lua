88fbb69e354472abd80acbe689217dc8e2983528
= <a@a.com>
2024-01-09 02:17:07 +0100

    Change surround plugin to prevent collison with hop(leap) plugins

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -25,7 +25,7 @@ return {
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.motion.hop-nvim" },
   { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.mini-surround"},
   { import = "astrocommunity.pack.full-dadbod" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
